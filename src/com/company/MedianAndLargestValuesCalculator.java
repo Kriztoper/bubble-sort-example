1c420b6254aee407dfe7be7227b11a2a9f224eac
Kriztoper Urmeneta <kriztoper.urmeneta@orangeandbronze.com>
2021-02-12 11:31:24 +0800

    Added initial blueprints

---
+++ src/com/company/MedianAndLargestValuesCalculator.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class MedianAndLargestValuesCalculator {
+
+    int[] values;
+
+    public MedianAndLargestValuesCalculator(int... values) {
+        this.values = values;
+    }
+
+    public int computeMedian() {
+        return 0;
+    }
+
+    public int computeLargest() {
+        return 0;
+    }
+}
---
+++ src/com/company/ValuesOrchestrator.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class ValuesOrchestrator {
+
+    private MedianAndLargestValuesCalculator calculator;
+
+    public ValuesOrchestrator() {
+        calculator = new MedianAndLargestValuesCalculator();
+    }
+
+    public void acceptInputValues() {
+
+    }
+
+    public void printMedian() {
+
+    }
+
+    public void printLargest() {
+
+    }
+
+}
