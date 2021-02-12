715c53cda02fd46a53984b04ba7b59f439f826d1
Kriztoper Urmeneta <kriztoper.urmeneta@orangeandbronze.com>
2021-02-12 12:23:07 +0800

    Implemented bubble sort

--- src/com/company/MedianAndLargestValuesCalculator.java
+++ src/com/company/MedianAndLargestValuesCalculator.java
@@ -10,6 +10,20 @@ public class MedianAndLargestValuesCalculator {
         this.values = values;
     }
 
+    private void bubbleSort() {
+        int size = values.length;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (values[j] > values[j+1]) {
+                    // swap
+                    Integer temp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = temp;
+                }
+            }
+        }
+    }
+
     public int computeMedian() {
         return 0;
     }
